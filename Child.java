dec45d3d27f87decddb5dbc22316210e9ca7a436
Petar Karadzhinov <86510538+PKaradjinov@users.noreply.github.com>
2021-11-05 16:32:32 +0200

    Delete Child.java

--- Child.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Child extends Person{
-
-    public Child(String name, String sex, String religion, String languageSpoken, String nationality, String countryOfResidence) {
-        super(name, sex, religion, languageSpoken, nationality, countryOfResidence);
-    }
-}
