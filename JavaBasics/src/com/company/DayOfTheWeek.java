307d81bbd14a4f41feaad22ff5cc97bf9f476e44
Petar Karadzhinov <86510538+PKaradjinov@users.noreply.github.com>
2021-11-11 11:20:21 +0200

    Update DayOfTheWeek.java

--- JavaBasics/src/com/company/DayOfTheWeek.java
+++ JavaBasics/src/com/company/DayOfTheWeek.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 //Write a program to print the days of the week based on the user digit 1 7 input. 
-//The message should be “The [user input digit] standard the day of the week is [the day of the week]” Hint: Use switch case.
+//The message should be “The [user input digit] st/nd/rd/th the day of the week is [the day of the week]” Hint: Use switch case.
 
 public class DayOfTheWeek {
 
@@ -14,25 +14,25 @@ public class DayOfTheWeek {
 
         switch (weekday) {
             case 1:
-                System.out.println("Monday");
+                System.out.println("The "+weekday+ "st the day of the week is Monday");
                 break;
             case 2:
-                System.out.println("Tuesday");
+                System.out.println("The "+weekday+ "nd the day of the week is Tuesday");
                 break;
             case 3:
-                System.out.println("Wednesday");
+                System.out.println("The "+weekday+ "rd the day of the week is Wednesday");
                 break;
             case 4:
-                System.out.println("Thursday");
+                System.out.println("The "+weekday+ "th the day of the week is Thursday");
                 break;
             case 5:
-                System.out.println("Friday");
+                System.out.println("The "+weekday+ "th the day of the week is Friday");
                 break;
             case 6:
-                System.out.println("Saturday");
+                System.out.println("The "+weekday+ "th the day of the week is Saturday");
                 break;
             case 7:
-                System.out.println("Sunday");
+                System.out.println("The "+weekday+ "nd the day of the week is Sunday");
                 break;
             default:
                 System.out.println("Please enter number from 1 to 7!");
