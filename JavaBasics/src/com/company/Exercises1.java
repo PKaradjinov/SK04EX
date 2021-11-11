56527365fe908535327f6f6b663e9c64cb272346
Petar Karadzhinov <86510538+PKaradjinov@users.noreply.github.com>
2021-11-11 12:23:27 +0200

    Update Exercises1.java

--- JavaBasics/src/com/company/Exercises1.java
+++ JavaBasics/src/com/company/Exercises1.java
@@ -7,17 +7,30 @@ public class Exercises1 {
 
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
-        System.out.print("Input the first number: ");
-        double x = in.nextDouble();
-        System.out.print("Input the Second number: ");
-        double y = in.nextDouble();
-        System.out.print("Input the third number: ");
-        double z = in.nextDouble();
-        System.out.print("The smallest value is " + smallest(x, y, z) + "\n");
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter the first number\n");
+        int num1 = scan.nextInt();
+        System.out.print("Enter the second number\n");
+        int num2 = scan.nextInt();
+        System.out.print("Enter the Third number\n");
+        int num3 = scan.nextInt();
+
+        smallestNumber(num1, num2, num3);
     }
 
-    public static double smallest(double x, double y, double z) {
-        return Math.min(Math.min(x, y), z);
+    static void smallestNumber(int num1, int num2, int num3) {//method definition
+        if (num1 <= num2 && num1 <= num3) {
+            System.out.println(num1 + " is the smallest number");
+
+        } else if (num2 <= num1 && num2 <= num3) {
+            System.out.println(num2 + " is the smallest number");
+
+        } else {
+            System.out.println(num3 + " is the smallest number");
+
+        }
+
     }
 }
