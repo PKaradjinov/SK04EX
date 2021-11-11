83ece4d517a23dd9ada34ecb5a5074c10b6a2025
Petar Karadzhinov <86510538+PKaradjinov@users.noreply.github.com>
2021-11-11 11:21:17 +0200

    Update BiggestNumberLoops.java

--- JavaBasics/src/com/company/BiggestNumberLoops.java
+++ JavaBasics/src/com/company/BiggestNumberLoops.java
@@ -1,5 +1,5 @@
 package com.company;
-
+//Find the largest number from a given array and print it in a console. Hint: use for loop
 public class BiggestNumberLoops {
     public static void main(String[] args) {
         int[] array = {1, 0, 6, 8, -4, 10};
