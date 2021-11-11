e31146933f88c553591843f943bac95ec10a2711
Petar Karadzhinov <86510538+PKaradjinov@users.noreply.github.com>
2021-11-11 10:58:59 +0200

    Update DayOfTheWeek.java

--- JavaBasics/src/com/company/DayOfTheWeek.java
+++ JavaBasics/src/com/company/DayOfTheWeek.java
@@ -3,44 +3,39 @@ package com.company;
 import java.util.Scanner;
 
 public class DayOfTheWeek {
-    //Write a program to print the days of the week based on the user digit 1 7 input. The message should be “The [user input digit]st nd rd th day of the week is [the day of the week]” Hint: Use switch case.
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Въведи номер: ");
+        System.out.println("Enter a number: ");
         int weekday = scanner.nextInt();
 
-        if (weekday == 1) {
-
-            System.out.println("Понеделник");
-
-        } else if (weekday == 2) {
-
-            System.out.println("Вторник");
-
-        } else if (weekday == 3) {
-
-            System.out.println("Сряда");
-
-        } else if (weekday == 4) {
-
-            System.out.println("Четвъртък");
-
-        } else if (weekday == 5) {
-
-            System.out.println("Петък");
-
-        } else if (weekday == 6) {
-
-            System.out.println("Събота");
-
-        } else if (weekday == 7) {
-
-            System.out.println("Неделя");
-
-        } else {
-
-            System.out.println("Моля въведете число между 1-7.");
+        switch (weekday) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Please enter number from 1 to 7!");
+                break;
         }
     }
 }
+
