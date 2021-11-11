0e245a4c77725bec54560ce22606df02d259839f
Petar Karadzhinov <86510538+PKaradjinov@users.noreply.github.com>
2021-11-11 11:02:37 +0200

    Update ArrayElements.java

--- JavaBasics/src/com/company/ArrayElements.java
+++ JavaBasics/src/com/company/ArrayElements.java
@@ -5,19 +5,16 @@ package com.company;
 class Test {
     static int arr[] = {112, 31, 4, 55};
 
-    // method for sum of elements in an array
     static int sum() {
         int sum = 0; // initialize sum
         int i;
 
-        // Iterate through all elements and add them to sum
         for (i = 0; i < arr.length; i++)
             sum += arr[i];
 
         return sum;
     }
 
-    // Driver method
     public static void main(String[] args) {
         System.out.println("Sum of given array is " + sum());
     }
